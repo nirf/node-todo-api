8cf90c3468ad0a59afa83b35d2ca2a1c2baa419a
home <fiegelshtein@gmail.com>
2017-07-31 19:43:25 +0300

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,6 +24,14 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((docs) => {
+        res.send({docs})
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Started on port ${PORT}`)
